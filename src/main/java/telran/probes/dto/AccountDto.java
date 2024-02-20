250a7a39e8602515651590394017f9636832db88
LudaTverday <Ludachka22@gmail.com>
2024-02-20 18:37:35 +0200

    changed method equals

--- src/main/java/telran/probes/dto/AccountDto.java
+++ src/main/java/telran/probes/dto/AccountDto.java
@@ -1,5 +1,8 @@
 package telran.probes.dto;
 
+import java.io.Serializable;
+import java.util.Objects;
+
 import jakarta.validation.constraints.*;
 import telran.probes.api.ErrorMessages;
 import telran.probes.api.ConstantsConfiguration;
@@ -7,6 +10,23 @@ import telran.probes.api.ConstantsConfiguration;
 public record AccountDto (@NotEmpty @Email(message = ErrorMessages.WRONG_EMAIL_FORMAT) String email, 
 		@NotNull @Size(min=ConstantsConfiguration.MIN_PASSWORD_SIZE, 
 		message = ErrorMessages.PASSWORD_SIZE_MESSAGE) String password,
-		@NotEmpty(message = ErrorMessages.EMPTY_ROLES_MESSAGE) String[] roles){
+		@NotEmpty(message = ErrorMessages.EMPTY_ROLES_MESSAGE) String[] roles) implements Serializable{
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(email);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		AccountDto other = (AccountDto) obj;
+		return Objects.equals(email, other.email);
+	}
 
 }
--- src/main/java/telran/probes/dto/SensorRangeDto.java
+++ src/main/java/telran/probes/dto/SensorRangeDto.java
@@ -1,12 +1,32 @@
 package telran.probes.dto;
 
+import java.util.Objects;
+
 import jakarta.validation.constraints.*;
 import telran.probes.api.ErrorMessages;
+
 
 public record SensorRangeDto (
 		@NotNull(message=ErrorMessages.MISSING_SENSOR_ID)
 		Long sensorId,
 		@NotNull(message = ErrorMessages.MISSING_SENSOR_RANGE)
 		 SensorRange sensorRange) {
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(sensorId);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		SensorRangeDto other = (SensorRangeDto) obj;
+		return Objects.equals(sensorId, other.sensorId);
+	}
 
 }
